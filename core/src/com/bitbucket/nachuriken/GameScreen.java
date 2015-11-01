983d8c02c85ac2083dd62b5e973cccb2af4b2ffa
Zemke <flzemke@gmail.com>
2015-11-01 07:46:47 +0100

    Added dynamic body

--- core/src/com/bitbucket/nachuriken/GameScreen.java
+++ core/src/com/bitbucket/nachuriken/GameScreen.java
@@ -31,8 +31,38 @@ public class GameScreen implements Screen {
 
     @Override
     public void show() {
-        
+        createPlayer();
         createFloor();
+    }
+
+    private void createPlayer() {
+        // First we create a body definition
+        BodyDef bodyDef = new BodyDef();
+        // We set our body to dynamic, for something like ground which doesn't move we would set it to StaticBody
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        // Set our body's starting position in the world
+        bodyDef.position.set(100, 300);
+
+        // Create our body in the world using our body definition
+        Body body = world.createBody(bodyDef);
+
+        // Create a circle shape and set its radius to 6
+        CircleShape circle = new CircleShape();
+        circle.setRadius(6f);
+
+        // Create a fixture definition to apply our shape to
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = circle;
+        fixtureDef.density = 0.5f;
+        fixtureDef.friction = 0.4f;
+        fixtureDef.restitution = 0.6f; // Make it bounce a little bit
+
+        // Create our fixture and attach it to the body
+        Fixture fixture = body.createFixture(fixtureDef);
+
+        // Remember to dispose of any shapes after you're done with them!
+        // BodyDef and FixtureDef don't need disposing, but shapes do.
+        circle.dispose();
     }
 
     private void createFloor() {
