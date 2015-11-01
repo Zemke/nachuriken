cd854da2cf40d4dd29958f75a7e6b164b9b0d191
Zemke <flzemke@gmail.com>
2015-11-01 06:17:50 +0100

    Implemented Screen

--- core/src/com/bitbucket/nachuriken/OurGame.java
+++ core/src/com/bitbucket/nachuriken/OurGame.java
@@ -1,12 +1,11 @@
 package com.bitbucket.nachuriken;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class OurGame extends ApplicationAdapter {
+public class OurGame extends Game {
 
     public static final String TITLE = "Nachuriken";
     public static final int WIDTH = 800;
@@ -14,19 +13,22 @@ public class OurGame extends ApplicationAdapter {
 
     SpriteBatch batch;
     Texture img;
+    BitmapFont font;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        img = new Texture("badlogic.jpg");
+        font = new BitmapFont();
+        this.setScreen(new MainMenuScreen(this));
     }
 
     @Override
     public void render() {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        batch.draw(img, 0, 0);
-        batch.end();
+        super.render();
+    }
+
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
     }
 }
