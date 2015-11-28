3dc3c087a86067c68f60d2ec2733006b020c6c13
Zemke <flzemke@gmail.com>
2015-11-28 18:55:55 +0100

    Started off with a newly generated project

--- desktop/src/com/bitbucket/nachuriken/desktop/DesktopLauncher.java
+++ desktop/src/com/bitbucket/nachuriken/desktop/DesktopLauncher.java
@@ -1,27 +1,12 @@
 package com.bitbucket.nachuriken.desktop;
 
-import com.badlogic.gdx.Application;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.bitbucket.nachuriken.OurGame;
+import com.bitbucket.nachuriken.Nachuriken;
 
 public class DesktopLauncher {
-
-    public static void main(String[] arg) {
-        LwjglApplicationConfiguration config = configureApplication();
-        new LwjglApplication(new OurGame(), config);
-
-        Gdx.app.setLogLevel(Application.LOG_DEBUG);
-    }
-
-    private static LwjglApplicationConfiguration configureApplication() {
-        LwjglApplicationConfiguration configuration = new LwjglApplicationConfiguration();
-
-        configuration.title = OurGame.TITLE;
-        configuration.width = OurGame.WIDTH;
-        configuration.height = OurGame.HEIGHT;
-
-        return configuration;
-    }
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Nachuriken(), config);
+	}
 }
